9fad9dc4ac2d693547feccecdeabf05b6e72389f
Antonin Portelli <antonin.portelli@me.com>
2020-01-10 17:40:39 +0000

    LatAnalyze update and update script

---
+++ update-latan.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git submodule update --remote
+INITDIR=$(pwd)
+cd dependencies/LatAnalyze/ci-scripts
+if [ "$(cat ${INITDIR}/dependencies/.latan 2>/dev/null)" != "$(git rev-parse HEAD)" ]; then
+  MACOSX_DEPLOYMENT_TARGET=10.14 ./install-latan.sh ../../prefix
+  git rev-parse HEAD > ${INITDIR}/dependencies/.latan
+fi
+cd ${INITDIR}
