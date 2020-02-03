1d147da189968b5ed8b63c86be9e447374a2fe13
Antonin Portelli <antonin.portelli@me.com>
2020-02-03 22:16:44 +0100

    2020 copyright update

--- src/data/dataoptiondialog.h
+++ src/data/dataoptiondialog.h
@@ -1,7 +1,7 @@
 /*
  * dataoptiondialog.h, part of Plateau
  *
- * Copyright (C) 2019 Antonin Portelli
+ * Copyright (C) 2019-2020 Antonin Portelli
  *
  * Plateau is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
