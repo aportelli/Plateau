348b8e7acaf4fa2d2ebc2830863ddc4334687c55
Antonin Portelli <antonin.portelli@me.com>
2021-11-17 15:56:18 +0000

    more lattice build script fix

--- update-latan.sh
+++ update-latan.sh
@@ -12,7 +12,7 @@ git submodule update --remote || echo "warning: LatAnalyze git update failed" 1>
 INITDIR=$(pwd)
 cd dependencies/LatAnalyze/ci-scripts
 if [ "$(cat ${INITDIR}/dependencies/.latan 2>/dev/null)" != "$(git rev-parse HEAD)" ]; then
-  MACOSX_DEPLOYMENT_TARGET=11 ./install-latan.sh ../../prefix ${NTASKS} 2>&1
+  ./install-latan.sh ../../prefix ${NTASKS} 2>&1
   git rev-parse HEAD > ${INITDIR}/dependencies/.latan
 fi
 cd ${INITDIR}
