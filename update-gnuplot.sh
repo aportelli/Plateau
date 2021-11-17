56aab4371493a029ddbbec9f9527f482f8d5a71a
Antonin Portelli <antonin.portelli@me.com>
2021-11-17 14:01:07 +0000

    script to update gnuplot Qt source

---
+++ update-gnuplot.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+root_dir=$(pwd -P)
+git clone https://github.com/gnuplot/gnuplot.git
+cd gnuplot/src/qtterminal
+cp QtGnuplotEvent.* QtGnuplotInstance.* QtGnuplotItems.* QtGnuplotScene.* QtGnuplotWidget.* ${root_dir}/src/gnuplot/
+cd ..
+cp mousecmn.h ${root_dir}/src
+cd ${root_dir}
+rm -rf gnuplot
